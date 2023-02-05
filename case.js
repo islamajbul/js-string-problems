5cf6cf57c39b79de31a50d4a45cc4d4abfe37b18
islamajabul <ajabulmd1@gmail.com>
2023-02-05 12:27:51 +0800

    case done

---
+++ case.js
@@ -0,0 +1,11 @@
+const userName = 'Ajabul Islam';
+const userInput = 'Ajabul islam';
+
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+if(userName.toLowerCase() == userInput.toLowerCase()){
+    console.log('valid user')
+}
+else{
+    console.log('invalid user')
+}
