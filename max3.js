00bb3f0e54d3951022cf206356bf8be183bd9492
islamajabul <ajabulmd1@gmail.com>
2023-02-06 11:51:39 +0800

    max & min

---
+++ max3.js
@@ -0,0 +1,23 @@
+const jim = 64;
+const dela = 69;
+const rinku = 77;
+
+/* -------Way 01----------- */
+if(jim > dela && jim > rinku){
+    console.log('Jim will get the cake');
+}
+else if (dela>jim && dela>rinku){
+    console.log('Dela will get the cake');
+}
+else{
+    console.log('Rinku will get the cake');
+}
+
+/* -------Way 02---------- */
+const maxNumber = Math.max(10,20,30);
+console.log('Maximum number is:',maxNumber);
+
+const minNumber = Math.min(10,20,30);
+console.log('Minimum number is:',minNumber);
+
+
