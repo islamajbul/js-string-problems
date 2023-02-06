23ce268a7df61eeea86a2fa82699da7313faae3b
islamajabul <ajabulmd1@gmail.com>
2023-02-06 11:16:37 +0800

    swap

---
+++ swap.js
@@ -0,0 +1,15 @@
+let first = 5;
+let second = 7;
+console.log(first,second);
+
+/* -----approach: 01------ */
+
+// const temp = first;
+// first = second;
+// second = temp;
+// console.log(first,second)
+
+/* -----approach: 02- Destructuring------ */
+
+[first,second] = [second,first];
+console.log(first,second);
