c5fa0ed899ee34b8610477cfd2b3e23947f76b51
islamajabul <ajabulmd1@gmail.com>
2023-02-06 13:29:07 +0800

    fibo

--- word-reverse.js
+++ word-reverse.js
@@ -1,11 +1,12 @@
 function reverseWords(str){
     const words = str.split(' ');
     const result = [];
-    for( let i = words.length-1, i>=0; i--){
+    for( let i = words.length-1; i >= 0; i--){
         const element = words[i];
         result.push(element);
     }
     const reversed = result.join(' ');
+    console.log(reversed);
     return reversed;
 }
 const myString = 'I am a good boy';
